ae9f664a7943db3b240747f44d370c3007f42ac7
Onojeta Brown <onosbrown.saved@gmail.com>
2023-03-31 06:13:27 +0100

    update

--- src/lib/components/result/remark.svelte
+++ src/lib/components/result/remark.svelte
@@ -83,9 +83,7 @@
       if (result.data.edit) {
         const index = remarks.findIndex((remark) => remark.id == result.data.remark.id);
         if (index === -1) return;
-        let newRemarks = remarks;
-        newRemarks[index] = result.data.remark;
-        remarks = [...newRemarks];
+        remarks[index] = result.data.remark;
         update();
         return;
       }
